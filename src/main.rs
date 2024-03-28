0389629d01efacbabd3f323388838e9a8fc94dfb
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2024-03-28 08:31:32 +0500

    feature: add playfair cipher

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-mod columnar_cipher;
+mod playfair_cipher;
 
-use columnar_cipher::{ decrypt, encrypt };
+use playfair_cipher::{ decrypt, encrypt };
 
 fn main() {
-    const PLAIN_TEXT: &str = "Zawwar";
-    const KEY: &str = "HACK";
-    let cipher_text = encrypt(PLAIN_TEXT, KEY);
-    let decipher_text = decrypt(&cipher_text, KEY);
+    const PLAIN_TEXT: &str = "gatlmzclrqtx";
+    const KEY: &str = "monarchy";
+    let cipher_text = encrypt(PLAIN_TEXT, KEY, 'z').unwrap();
+    let decipher_text = decrypt(&cipher_text, KEY).unwrap();
     println!("Plain Text:- {}", PLAIN_TEXT);
     println!("Key:- {}", KEY);
     println!("Cipher Text:- {}", cipher_text);
